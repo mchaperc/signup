20bb628be48454266c386593bf9402a7df6fc1f4
Matt Chastain <matthewdchastain@gmail.com>
2017-04-18 21:19:21 -0400

    done

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import falcon
 from falcon_cors import CORS
 from pymongo import MongoClient
-from bson import Binary, Code
 from bson.json_util import dumps
+import json
 
 connection = MongoClient('ds161950.mlab.com', 61950)
 db = connection['heroku_08bc3pdq']
@@ -11,21 +11,34 @@ db.authenticate('heroku_08bc3pdq', 'jobkq04sg3dsg8881uncchd5d0')
 auditions = db.posts
 
 
-cors = CORS(allow_origins_list=['https://mchaperc.github.io/signup'])
+cors = CORS(allow_origins_list=['https://mchaperc.github.io', 'file:///Users/mattchastain/Desktop/dev'])
 public_cors = CORS(allow_all_origins=True)
 
 
-class GetExistingDoc(object):
+class GetStudentSignups(object):
     cors = public_cors
     def on_get(self, req, resp):
         """Handles GET requests"""
         existing_doc = auditions.find_one({'ensemble': 'Singers'})
         resp.status = falcon.HTTP_200
+        print(resp.status)
         resp.body = dumps(existing_doc)
 
 
+class PostStudentSignup(object):
+    cors = public_cors
+    def on_post(self, req, resp):
+        chunk = req.stream.read(4096).decode('utf-8')
+        if not chunk:
+            return
+        print(chunk, json.loads(chunk))
+        auditions.update({'_id': 1}, {"$set": {'time_slots': json.loads(chunk)}}, upsert=False)
+
+
 app = falcon.API(middleware=[cors.middleware])
-get_docs = GetExistingDoc()
+get_signups = GetStudentSignups()
+post_signups = PostStudentSignup()
 
 
-app.add_route('/get_docs/', get_docs)
+app.add_route('/get_docs/', get_signups)
+app.add_route('/post_docs/', post_signups)
--- main.js
+++ main.js
@@ -2,13 +2,68 @@
  * Created by mattchastain on 4/17/17.
  */
 $(document).ready( () => {
-    marmottajax({
-        url: `http://127.0.0.1:8000/get_docs/`,
-        method: 'get',
-    }).success(function(result) {
-        result = JSON.parse(result);
-        console.log(result);
-    }).error(function(message) {
-        console.log(message);
+    let formData;
+    // marmottajax({
+    //     url: `http://127.0.0.1:8000/get_docs/`,
+    //     method: 'get',
+    // }).success(function(result) {
+    //     formData = JSON.parse(result);
+    //     let i = 1;
+    //     formData && formData.time_slots && formData.time_slots.length && formData.time_slots.forEach( (student) => {
+    //         if ( student[`student${i}`]) {
+    //             let currentStudent = $(`#student${i}`);
+    //             currentStudent.attr('disabled', true);
+    //             currentStudent.val(student[`student${i}`]);
+    //         }
+    //     });
+    // }).error(function(message) {
+    //     console.log(message);
+    // });
+    axios.get('http://127.0.0.1:8000/get_docs/')
+    .then(function (response) {
+        formData = response.data;
+        console.log(formData);
+        let i = 1;
+        formData && formData.time_slots && formData.time_slots.length && formData.time_slots.forEach( (student) => {
+            if ( student[`student${i}`]) {
+                console.log( `student${i}`, student );
+                let currentStudent = $(`#student${i}`);
+                currentStudent.attr('disabled', true);
+                currentStudent.val(student[`student${i}`]);
+            }
+            i++;
+        });
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+    $('#saveSignup').on('click', (e) => {
+        $('input').each( (index, val) => {
+            if ( $(val).val() ) {
+                formData.time_slots.map( ( student ) => {
+                   if (student.hasOwnProperty(val.id)) {
+                       student.read_only = true;
+                       student[val.id] = $(val).val()
+                    }
+                    return student
+                });
+            }
+        });
+        console.log(formData);
+        axios({
+            method: 'post',
+            url: 'http://127.0.0.1:8000/post_docs/',
+            data: JSON.stringify(formData && formData.time_slots)
+        });
+        // marmottajax({
+        //     url: 'http://127.0.0.1:8000/post_docs/',
+        //     method: "post",
+        //     formData,
+        // }).success(function(result) {
+        //     // result
+        // }).error(function(message) {
+        //     // message
+        // });
     });
 });
